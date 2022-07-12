c79ae2d148c121e75aa7cd0accbad1f859efdc92
John Doe <69335633+Jejebecarte@users.noreply.github.com>
2022-07-12 21:19:34 +1000

    feat: add Footer

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { MantineProvider, ColorScheme, ColorSchemeProvider } from "@mantine/core";
 import { useLocalStorageValue, useHotkeys } from "@mantine/hooks";
 import { Header } from "./Header/Header";
+import { Footer } from "./Footer/Footer";
 
 const THEME_KEY = "mantine-color-scheme";
 
@@ -21,6 +22,7 @@ export function Layout({ children, noMarginals }: LayoutProps) {
 			<MantineProvider theme={{ colorScheme }} withNormalizeCSS withGlobalStyles>
 				{!noMarginals && <Header />}
 				<main>{children}</main>
+				{!noMarginals && <Footer />}
 			</MantineProvider>
 		</ColorSchemeProvider>
 	);
