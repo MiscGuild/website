5757f5b5d59cb81068361adc5ae70d4cdf7e1800
John Doe <69335633+Jejebecarte@users.noreply.github.com>
2022-07-12 18:26:11 +1000

    feat: add default seo values

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,9 +6,9 @@ import favicon from "../public/favicon.svg";
 
 export default function App({ Component, pageProps }: AppPropsWithMeta) {
 	const meta = {
-		title: "",
-		description: "",
-		image: "./",
+		title: "Miscellaneous",
+		description: "Miscellaneous - a welcoming, friendly, and active Hypixel guild for the best of the best.",
+		image: favicon.src,
 		type: "website",
 		...Component.customSeo,
 	};
@@ -21,10 +21,10 @@ export default function App({ Component, pageProps }: AppPropsWithMeta) {
 
 				<meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
 				<meta name="robots" content="index,follow" />
-				<meta name="keywords" content="" />
+				<meta name="keywords" content="hypixel, hypixel guild, miscellaneous, minecraft" />
 				<meta name="description" content={meta.description} />
 
-				<meta property="og:site_name" content="" />
+				<meta property="og:site_name" content="Miscellaneous" />
 				<meta property="og:title" content={meta.title} />
 				<meta property="og:description" content={meta.description} />
 				<meta property="og:type" content={meta.type} />
