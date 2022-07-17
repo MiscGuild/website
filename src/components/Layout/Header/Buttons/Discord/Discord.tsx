3dc6c86972b0a1a97f46b7f38f43f86d82d0dad3
John Doe <69335633+Jejebecarte@users.noreply.github.com>
2022-07-17 20:50:45 +1000

    fix: add aria-label and title to Discord button

--- src/components/Layout/Header/Buttons/Discord/Discord.tsx
+++ src/components/Layout/Header/Buttons/Discord/Discord.tsx
@@ -4,7 +4,7 @@ import { HeaderButton } from "../HeaderButton";
 export function Discord() {
 	return (
 		<a href="https://discord.com/invite/bHFWukp" target="_blank" rel="noreferrer">
-			<HeaderButton Icon={FaDiscord} />
+			<HeaderButton Icon={FaDiscord} aria-label="Miscellaneous Discord" title="Discord" />
 		</a>
 	);
 }
