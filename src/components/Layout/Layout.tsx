30e90811176d8676ed923562e7e66a38817ddf06
John Doe <69335633+Jejebecarte@users.noreply.github.com>
2022-07-17 20:18:39 +1000

    fix: fix prop className did not match hydration error on light mode reload

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,34 +1,54 @@
 import { ColorScheme, ColorSchemeProvider, MantineProvider } from "@mantine/core";
-import { useHotkeys, useLocalStorageValue } from "@mantine/hooks";
+import { CustomSeo } from "../../pages/_app";
 import { Footer } from "./Footer/Footer";
 import { HEADER_HEIGHT } from "./Header/Header.styles";
+import Head from "next/head";
 import { Header } from "./Header/Header";
+import React from "react";
+import favicon from "../../public/favicon.svg";
 
-const THEME_KEY = "mantine-color-scheme";
-
-export function Layout({ children, noMarginals }: LayoutProps) {
-	const [colorScheme, setColorScheme] = useLocalStorageValue<ColorScheme>({
-		key: THEME_KEY,
-		defaultValue: "dark",
-	});
-
-	const toggleColorScheme = (value?: ColorScheme) =>
-		setColorScheme(value || (colorScheme === "dark" ? "light" : "dark"));
-
-	useHotkeys([["mod + shift + L", () => toggleColorScheme()]]);
+export function Layout({ children, customSeo, colorScheme, noMarginals, toggleColorScheme }: LayoutProps) {
+	const meta = {
+		title: "Miscellaneous",
+		description: "Miscellaneous - a welcoming, friendly, and active Hypixel guild for the best of the best.",
+		image: favicon.src,
+		type: "website",
+		...customSeo,
+	};
 
 	return (
-		<ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
-			<MantineProvider theme={{ colorScheme, primaryColor: "grape" }} withNormalizeCSS withGlobalStyles>
-				{!noMarginals && <Header />}
-				<main style={{ paddingTop: !noMarginals ? HEADER_HEIGHT : 0 }}>{children}</main>
-				{!noMarginals && <Footer />}
-			</MantineProvider>
-		</ColorSchemeProvider>
+		<>
+			<Head>
+				<title>{meta.title}</title>
+				<link rel="shortcut icon" href={favicon.src} />
+
+				<meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+				<meta name="robots" content="index,follow" />
+				<meta name="keywords" content="hypixel, hypixel guild, miscellaneous, minecraft" />
+				<meta name="description" content={meta.description} />
+
+				<meta property="og:site_name" content="Miscellaneous" />
+				<meta property="og:title" content={meta.title} />
+				<meta property="og:description" content={meta.description} />
+				<meta property="og:type" content={meta.type} />
+				<meta property="og:image" content={meta.image} />
+			</Head>
+
+			<ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
+				<MantineProvider theme={{ colorScheme, primaryColor: "grape" }} withNormalizeCSS withGlobalStyles>
+					{!noMarginals && <Header />}
+					<main style={{ paddingTop: !noMarginals ? HEADER_HEIGHT : 0 }}>{children}</main>
+					{!noMarginals && <Footer />}
+				</MantineProvider>
+			</ColorSchemeProvider>
+		</>
 	);
 }
 
-interface LayoutProps {
+type LayoutProps = {
 	children?: React.ReactNode;
+	customSeo?: CustomSeo;
+	colorScheme: ColorScheme;
 	noMarginals?: boolean;
-}
+	toggleColorScheme: (colorScheme?: ColorScheme) => void;
+};
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,53 +1,44 @@
+import { GetServerSidePropsContext, NextPage } from "next";
+import { getCookie, setCookies } from "cookies-next";
 import { AppProps } from "next/app";
-import Head from "next/head";
+import { ColorScheme } from "@mantine/core";
 import { Layout } from "../components/Layout/Layout";
-import { NextPage } from "next";
-import favicon from "../public/favicon.svg";
+import { useState } from "react";
 
-export default function App({ Component, pageProps }: CustomAppProps) {
-	const meta = {
-		title: "Miscellaneous",
-		description: "Miscellaneous - a welcoming, friendly, and active Hypixel guild for the best of the best.",
-		image: favicon.src,
-		type: "website",
-		...Component.customSeo,
+export default function App(props: CustomAppProps) {
+	const { Component, pageProps } = props;
+	const [colorScheme, setColorScheme] = useState<ColorScheme>(props.colorScheme);
+
+	const toggleColorScheme = (value?: ColorScheme) => {
+		const nextColorScheme = value || (colorScheme === "dark" ? "light" : "dark");
+		setColorScheme(nextColorScheme);
+		setCookies("mantine-color-scheme", nextColorScheme, { maxAge: 60 * 60 * 24 * 30 });
 	};
 
 	return (
-		<>
-			<Head>
-				<title>{meta.title}</title>
-				<link rel="shortcut icon" href={favicon.src} />
-
-				<meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
-				<meta name="robots" content="index,follow" />
-				<meta name="keywords" content="hypixel, hypixel guild, miscellaneous, minecraft" />
-				<meta name="description" content={meta.description} />
-
-				<meta property="og:site_name" content="Miscellaneous" />
-				<meta property="og:title" content={meta.title} />
-				<meta property="og:description" content={meta.description} />
-				<meta property="og:type" content={meta.type} />
-				<meta property="og:image" content={meta.image} />
-			</Head>
-
-			<Layout>
-				<Component {...pageProps} />
-			</Layout>
-		</>
+		<Layout colorScheme={colorScheme} noMarginals={Component.noMarginals} toggleColorScheme={toggleColorScheme}>
+			<Component {...pageProps} />
+		</Layout>
 	);
 }
 
+App.getInitialProps = ({ ctx }: { ctx: GetServerSidePropsContext }) => ({
+	colorScheme: getCookie("mantine-color-scheme", ctx) || "dark",
+});
+
 export type PageProps = NextPage & {
 	noMarginals?: boolean;
-	customSeo?: {
-		title: string;
-		description: string;
-		image: string;
-		type: string;
-	};
+	customSeo?: CustomSeo;
+};
+
+export type CustomSeo = {
+	title: string;
+	description: string;
+	image: string;
+	type: string;
 };
 
 type CustomAppProps = AppProps & {
 	Component: PageProps;
+	colorScheme: ColorScheme;
 };
